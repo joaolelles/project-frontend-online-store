6ef4bb3c450ab765acf919977550e07917678151
Joseane Oliveira <joseaneoliveira.joli@gmail.com>
2022-10-07 17:02:32 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+// import { BrowseRouter } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+// import * as api from './services/api';
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
         </a>
       </header>
     </div>
+
   );
 }
 
